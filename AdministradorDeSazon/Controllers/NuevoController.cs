9df57ce765329efa641f53ce72171c7b909c0544
dbarriguete <dbarriguete@gmail.com>
2020-02-29 23:41:22 -0600

    Updating project

---
+++ AdministradorDeSazon/Controllers/NuevoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdministradorDeSazon.Controllers
+{
+    public class NuevoController : Controller
+    {
+        // GET: Nuevo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Administrador()
+        {
+            ViewBag.Message = "Inicia tu operación con Administrador del Sazón.";
+
+            return View();
+        }
+
+        public ActionResult RegistroNegocio()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult RegistroSucursal()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
